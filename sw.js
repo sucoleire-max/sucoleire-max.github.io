f067c676905b0ed01ef1c4d2a89bca98ec96e2d1
sucoleire-max <sucoleire@gmail.com>
2026-09-06 13:19:44 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Clear all caches
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
